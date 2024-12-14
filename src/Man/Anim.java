3aab206a3db69d638e143d2cfb9c99be7d3529ff
Klaus-22 <androa100@yahoo.com>
2024-12-14 20:59:21 +0200

    Different Mountain approach

--- src/Man/Anim.java
+++ src/Man/Anim.java
@@ -1,25 +1,33 @@
 package Man;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package project;
+
 import com.sun.opengl.util.*;
 import java.awt.*;
 import javax.media.opengl.*;
 import javax.swing.*;
-import screens.SplashScreen;
 
 public class Anim extends JFrame {
 
     public static void main(String[] args) {
-        SplashScreen splash = new SplashScreen();
-        splash.showSplashScreen();
-
-        SwingUtilities.invokeLater(() -> new Anim());
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i%4);
+//        }
+        new Anim();
     }
+
 
     public Anim() {
         GLCanvas glcanvas;
         Animator animator;
 
-        AnimListener listener = new AnimGLEventListener4();
+        
+        AnimListener listener = new AnimGLEventListener();
         glcanvas = new GLCanvas();
         glcanvas.addGLEventListener(listener);
         glcanvas.addKeyListener(listener);
@@ -30,7 +38,7 @@ public class Anim extends JFrame {
 
         setTitle("Anim Test");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
+        setSize(700, 700);
         setLocationRelativeTo(null);
         setVisible(true);
         setFocusable(true);
